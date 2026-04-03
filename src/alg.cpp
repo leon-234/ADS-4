135609f8409948bcfef51ccad6ef6ef1ba206453
leon-234 <rust824@mail.ru>
2026-04-03 21:12:28 +0300

    Refactor nested loops and improve readability

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,9 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
     int count = 0;
-    for (int i = 0; i < len; i++)
-    {
-        for (int j = i + 1; j < len; j++)
-        {
-            if (arr[i] + arr[j] == value)
-            {
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value) {
                 count++;
             }
         }
@@ -14,10 +11,8 @@ int countPairs1(int *arr, int len, int value) {
     return count;
 }
 
-int binarySearch(int* arr, int left, int right, int target)
-{
-    while (left <= right)
-    {
+int binarySearch(int* arr, int left, int right, int target) {
+    while (left <= right) {
         int mid = left + (right - left) / 2;
         if (arr[mid] == target)
             return mid;
@@ -32,21 +27,17 @@ int binarySearch(int* arr, int left, int right, int target)
 
 int countPairs2(int *arr, int len, int value) {
     int count = 0;
-    for (int i = 0; i < len; i++)
-    {
+    for (int i = 0; i < len; i++) {
         int target = value - arr[i];
         int pos = binarySearch(arr, i + 1, len - 1, target);
-        if (pos != -1)
-        {
+        if (pos != -1) {
             int temp = pos;
-            while (temp < len && arr[temp] == target)
-            {
+            while (temp < len && arr[temp] == target) {
                 count++;
                 temp++;
             }
             temp = pos - 1;
-            while (temp > i && arr[temp] == target)
-            {
+            while (temp > i && arr[temp] == target) {
                 count++;
                 temp--;
             }
@@ -58,40 +49,31 @@ int countPairs3(int *arr, int len, int value) {
     int left = 0;
     int right = len - 1;
     int count = 0;
-    while (left < right)
-    {
+    while (left < right) {
         int sum = arr[left] + arr[right];
 
-        if (sum < value)
-        {
+        if (sum < value) {
             left++;
         }
-        else if (sum > value)
-        {
+        else if (sum > value) {
             right--;
         }
-        else
-        {
-
-            if (arr[left] == arr[right])
-            {
+        else {
+            if (arr[left] == arr[right]) {
                 int n = right - left + 1;
                 count += (n * (n - 1)) / 2;
                 break;
             }
-            else
-            {
+            else {
                 int leftVal = arr[left];
                 int rightVal = arr[right];
                 int leftCount = 0;
                 int rightCount = 0;
-                while (left <= right && arr[left] == leftVal)
-                {
+                while (left <= right && arr[left] == leftVal) {
                     left++;
                     leftCount++;
                 }
-                while (right >= left && arr[right] == rightVal)
-                {
+                while (right >= left && arr[right] == rightVal) {
                     right--;
                     rightCount++;
                 }
